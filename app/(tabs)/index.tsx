6d56b170a970e2d613f5ca8c8650c3018817b155
louis <louis.hocquet@sfr.fr>
2026-01-20 15:55:10 +0100

    Second Iteration - Add Home and Settings styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,35 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
 import useTheme from "@/hooks/useTheme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+
+  // const styles = createStyles(colors);
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>Hello world</Text>
+    <SafeAreaView style={homeStyles.safeArea}>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle Dark mode</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    fontSize: 20,
-  },
-});
+// const createStyles = (colors: ColorScheme) => {
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       justifyContent: "center",
+//       alignItems: "center",
+//       gap: 10,
+//       backgroundColor: colors.bg,
+//     },
+//     content: {
+//       fontSize: 20,
+//     },
+//   });
+//   return styles;
+// };
