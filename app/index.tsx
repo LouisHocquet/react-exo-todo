33007b72356fe5f9b45002b75ffce906529fb54b
louis <louis.hocquet@sfr.fr>
2026-01-20 10:12:18 +0100

    Project Setup - clean default files

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about">Yo</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red",
+      }
+})
